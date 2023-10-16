8ea2e5dab8bfb0732cd70ad33db78839ea085d16
Lavina-23 <lavinakohai3227@gmail.com>
2023-10-16 08:53:20 +0700

    9. Bloc Provider Value

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:learn_bloc/bloc/counter.dart';
+import 'package:learn_bloc/other/other.dart';
 import 'package:learn_bloc/pages/data_counter.dart';
 
 class Homepage extends StatelessWidget {
@@ -9,9 +10,29 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
   Counter mycounter = BlocProvider.of<Counter>(context);
+  // Counter mycounter = context.read<Counter>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("DEPENDENCY INJECTION"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => BlocProvider.value(
+                value: mycounter,
+                child: const OtherPage(),
+              ),
+              
+              // BlocProvider(
+              //   create: (context) => mycounter,
+              //   child: const OtherPage(),
+              // ),
+            ),
+          );
+        },
+        child: const Icon(Icons.arrow_forward),
       ),
       body: Center(
         child: Row(
